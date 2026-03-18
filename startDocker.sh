b8ad16d478acc0837b1bab0887bfe22de1396600
Andy Cotton <acotton@pih.org>
2026-03-18 14:27:58 -0400

    Update dependencies and add docker components

---
+++ startDocker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+ENV_FILE=.env
+if [ ! -f "$ENV_FILE" ]; then
+    echo "Environment file not found. Creating local .env from dev.env"
+    cp dev.env .env
+fi
+BUILD_DIR=./target/distro/web
+if [ ! -d "$BUILD_DIR" ]; then
+    echo "Build directory not found - running mvn clean install"
+    mvn clean install
+fi
+docker compose up --build
